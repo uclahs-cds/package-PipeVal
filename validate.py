ea2f110a4636e1ff9fe92f9b84d01f5a44cf89d9
Gina Kim <ginakkiim@gmail.com>
2020-06-29 18:08:27 -0700

    file existence check

--- validate.py
+++ validate.py
@@ -1,5 +1,13 @@
+from pathlib import Path
 import argparse
+
 parser = argparse.ArgumentParser()
-parser.add_argument("path", help="full path of parameter")
+parser.add_argument("path", help="full path of parameter", type=str)
 args = parser.parse_args()
-print(args.path)
+path = Path(args.path)
+
+# check existence
+if path.exists():
+    print("File extension is: " + path.suffix)
+else:
+    print("File does not exist.")
