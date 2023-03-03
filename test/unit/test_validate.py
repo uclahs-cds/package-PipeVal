a063c7e8198383314ea5a63d3acfe6e9ce5f7e6f
Yash Patel <yashpatel@mednet.ucla.edu>
2023-03-02 16:10:14 -0800

    Add tests for check_extension

--- test/unit/test_validate.py
+++ test/unit/test_validate.py
@@ -3,7 +3,12 @@ import mock
 import pytest
 
 from validate.validate import (
-    detect_file_type_and_extension
+    detect_file_type_and_extension,
+    check_extension
+)
+from validate.files import (
+    check_compressed,
+    path_exists
 )
 
 def test__detect_file_type_and_extension__detects_correct_file_type():
@@ -25,5 +30,21 @@ def test__detect_file_type_and_extension__detects_correct_unknown_file_type():
 
     assert extension == ''.join(expected_extensions)
     assert file_type == expected_file_type
+
+def test__check_extension__correct_file_type():
+    test_extension = '.vcf.gz'
+    expected_type = 'file-vcf'
+
+    file_type = check_extension(test_extension)
+
+    assert file_type == expected_type
+
+def test__check_extension__correct_unknown_file_type():
+    test_extension = '.unknown'
+    expected_type = 'file-unknown'
+
+    file_type = check_extension(test_extension)
+
+    assert file_type == expected_type
 
 
