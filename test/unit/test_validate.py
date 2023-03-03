c336b5d546de24eed10ac40569bd5e2d40b398c9
Yash Patel <yashpatel@mednet.ucla.edu>
2023-03-02 16:57:03 -0800

    Add tests for validate_bam_files

--- test/unit/test_validate.py
+++ test/unit/test_validate.py
@@ -1,7 +1,9 @@
 from pathlib import Path
+from unittest.mock import Mock
 import mock
 import pytest
 import warnings
+import pysam
 
 from validate.validate import (
     detect_file_type_and_extension,
@@ -10,6 +12,10 @@ from validate.validate import (
 from validate.files import (
     check_compressed,
     path_exists
+)
+from validate.validators.bam import (
+    validate_bam_file,
+    check_bam_index
 )
 
 def test__detect_file_type_and_extension__detects_correct_file_type():
@@ -77,3 +83,27 @@ def test__check_compressed__raises_warning_for_uncompressed_path(mock_path):
     with warnings.catch_warnings():
         warnings.filterwarnings("error")
         check_compressed(mock_path, test_extension)
+
+@mock.patch('validate.validators.bam.pysam')
+def test__validate_bam_file__empty_bam_file(mock_pysam):
+    mock_alignment_file = Mock()
+    mock_alignment_file.head.return_value = iter([])
+    mock_pysam.AlignmentFile.return_value = mock_alignment_file
+
+    test_path = Path('empty/valid/bam')
+
+    with pytest.raises(ValueError) as ve_error:
+        validate_bam_file(test_path)
+
+    assert str(ve_error.value) == f'pysam bam check failed. No reads in {test_path}'
+
+@mock.patch('validate.validators.bam.Path', autospec=True)
+def test__validate_bam_file__quickcheck_fails(mock_path):
+    mock_path.exists.return_value = False
+
+    with pytest.raises(ValueError) as ve_error:
+        validate_bam_file(mock_path)
+
+    assert str(ve_error.value).startswith('samtools bam check failed.')
+
+
