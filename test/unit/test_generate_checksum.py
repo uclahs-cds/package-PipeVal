bfda0bd5f3f38d8178c8d285cd94d22043de4828
Yash Patel <yashpatel@mednet.ucla.edu>
2023-03-03 16:20:42 -0800

    Use pytest.mark.parametrize

--- test/unit/test_generate_checksum.py
+++ test/unit/test_generate_checksum.py
@@ -43,46 +43,36 @@ def test__validate_checksums__error_raised_when_comparison_fails(mock_path, mock
     with pytest.raises(IOError):
         validate_checksums(mock_path)
 
+@pytest.mark.parametrize('hash_type', [('md5'), ('sha512')])
+@mock.patch('generate_checksum.checksum.generate_sha512')
 @mock.patch('generate_checksum.checksum.generate_md5')
 @mock.patch('generate_checksum.checksum.Path', autospec=True)
-def test__compare_hash__compares_correct_md5_checksums(mock_path, mock_generate_md5):
+def test__compare_hash__compares_correct_checksums(
+    mock_path,
+    mock_generate_md5,
+    mock_generate_sha512,
+    hash_type):
     checksum_to_compare = '123456abcd'
-    hash_type = 'md5'
     mock_path.read_text.return_value = f'{checksum_to_compare} fname'
 
     mock_generate_md5.return_value = checksum_to_compare
-
-    assert compare_hash(hash_type, mock_path, mock_path)
-
-@mock.patch('generate_checksum.checksum.generate_sha512')
-@mock.patch('generate_checksum.checksum.Path', autospec=True)
-def test__compare_hash__compares_correct_sha512_checksums(mock_path, mock_generate_sha512):
-    checksum_to_compare = '123456abcd'
-    hash_type = 'sha512'
-    mock_path.read_text.return_value = f'{checksum_to_compare} fname'
-
     mock_generate_sha512.return_value = checksum_to_compare
 
     assert compare_hash(hash_type, mock_path, mock_path)
 
+@pytest.mark.parametrize('hash_type', [('md5'), ('sha512')])
+@mock.patch('generate_checksum.checksum.generate_sha512')
 @mock.patch('generate_checksum.checksum.generate_md5')
 @mock.patch('generate_checksum.checksum.Path', autospec=True)
-def test__compare_hash__compares_incorrect_md5_checksums(mock_path, mock_generate_md5):
+def test__compare_hash__compares_incorrect_checksums(
+    mock_path,
+    mock_generate_md5,
+    mock_generate_sha512,
+    hash_type):
     checksum_to_compare = '123456abcd'
-    hash_type = 'md5'
     mock_path.read_text.return_value = f'{checksum_to_compare} fname'
 
     mock_generate_md5.return_value = 'wrong'
-
-    assert not compare_hash(hash_type, mock_path, mock_path)
-
-@mock.patch('generate_checksum.checksum.generate_sha512')
-@mock.patch('generate_checksum.checksum.Path', autospec=True)
-def test__compare_hash__compares_incorrect_sha512_checksums(mock_path, mock_generate_sha512):
-    checksum_to_compare = '123456abcd'
-    hash_type = 'sha512'
-    mock_path.read_text.return_value = f'{checksum_to_compare} fname'
-
     mock_generate_sha512.return_value = 'wrong'
 
     assert not compare_hash(hash_type, mock_path, mock_path)
