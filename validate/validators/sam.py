2105103f06c81348224a7db1f297c7ddfbddf454
Yash Patel <yashpatel@mednet.ucla.edu>
2023-03-08 12:24:38 -0800

    Update variable name

---
+++ validate/validators/sam.py
@@ -0,0 +1,22 @@
+'''Helper methods for SAM file validation'''
+
+from pathlib import Path
+
+import pysam
+
+def validate_sam_file(path):
+    '''Validates sam file'''
+    try:
+        pysam.quickcheck(str(path))
+    except pysam.SamtoolsError as err:
+        raise ValueError("samtools sam check failed. " + str(err)) from err
+
+    sam_head: pysam.IteratorRowHead = pysam.AlignmentFile(str(path)).head(1)
+    if next(sam_head, None) is None: #if the iterator is exhausted, next() returns None
+        raise ValueError("pysam sam check failed. No reads in " + str(path))
+
+    return True
+
+def check_sam(path:Path):
+    ''' Validation for SAMs '''
+    validate_sam_file(path)
