1f1aa58ea9885dc75aab8b74549c430476d7469c
Yash Patel <yashpatel@mednet.ucla.edu>
2023-03-08 17:23:24 -0800

    Add tests

--- test/unit/test_validate.py
+++ test/unit/test_validate.py
@@ -10,7 +10,8 @@ import pytest
 from validate.validate import (
     detect_file_type_and_extension,
     check_extension,
-    run_validate
+    run_validate,
+    validate_file
 )
 from validate.files import (
     check_compressed,
@@ -22,6 +23,13 @@ from validate.validators.bam import (
 )
 from validate.validators.vcf import (
     validate_vcf_file
+)
+from validate.validators.sam import (
+    validate_sam_file
+)
+from validate.validators.cram import (
+    validate_cram_file,
+    check_cram_index
 )
 
 @pytest.mark.parametrize(
@@ -116,6 +124,66 @@ def test__check_bam_index__index_check_pass(mock_pysam):
 
     check_bam_index('/some/file')
 
+@mock.patch('validate.validators.bam.pysam')
+def test__validate_sam_file__empty_sam_file(mock_pysam):
+    mock_alignment_file = Mock()
+    mock_alignment_file.head.return_value = iter([])
+    mock_pysam.AlignmentFile.return_value = mock_alignment_file
+
+    test_path = Path('empty/valid/sam')
+
+    with pytest.raises(ValueError):
+        validate_sam_file(test_path)
+
+@mock.patch('validate.validators.sam.Path', autospec=True)
+def test__validate_sam_file__quickcheck_fails(mock_path):
+    mock_path.exists.return_value = False
+
+    with pytest.raises(ValueError):
+        validate_sam_file(mock_path)
+
+@pytest.mark.parametrize(
+    'test_reference',
+    [
+        (None),
+        ('ref')
+    ]
+)
+@mock.patch('validate.validators.cram.pysam')
+def test__validate_cram_file__empty_cram_file(mock_pysam, test_reference):
+    mock_alignment_file = Mock()
+    mock_alignment_file.head.return_value = iter([])
+    mock_pysam.AlignmentFile.return_value = mock_alignment_file
+
+    test_path = Path('empty/valid/cram')
+
+    with pytest.raises(ValueError):
+        validate_cram_file(test_path, test_reference)
+
+@mock.patch('validate.validators.cram.Path', autospec=True)
+def test__validate_cram_file__quickcheck_fails(mock_path):
+    mock_path.exists.return_value = False
+
+    with pytest.raises(ValueError):
+        validate_cram_file(mock_path)
+
+@mock.patch('validate.validators.cram.pysam', autospec=True)
+def test__check_cram_index__no_index_file_error(mock_pysam):
+    mock_alignment_file = Mock()
+    mock_alignment_file.check_index.side_effect = ValueError('no')
+    mock_pysam.AlignmentFile.return_value = mock_alignment_file
+
+    with pytest.raises(FileNotFoundError):
+        check_cram_index('/some/file')
+
+@mock.patch('validate.validators.cram.pysam', autospec=True)
+def test__check_cram_index__index_check_pass(mock_pysam):
+    mock_alignment_file = Mock()
+    mock_alignment_file.check_index.return_value = True
+    mock_pysam.AlignmentFile.return_value = mock_alignment_file
+
+    check_cram_index('/some/file')
+
 @mock.patch('validate.validators.vcf.subprocess.call')
 def test__validate_vcf_file__fails_vcf_validation(mock_call):
     mock_call.return_value = 1
@@ -161,3 +229,36 @@ def test__run_validate__fails_with_failing_checks(
     with pytest.raises(SystemExit) as pytest_exit:
         run_validate(test_args)
     assert pytest_exit.value.code == expected_code
+
+@mock.patch('validate.validate.path_exists')
+def test__validate_file__errors_with_invalid_extension(mock_path_exists):
+    mock_path_exists.return_value = True
+
+    with pytest.raises(TypeError):
+        validate_file('', 'file-test', '', None)
+
+@pytest.mark.parametrize(
+    'test_file_types',
+    [
+        ('file-vcf'),
+        ('file-fastq'),
+        ('file-bed')
+    ]
+)
+@mock.patch('validate.validate.path_exists')
+@mock.patch('validate.validate.check_compressed')
+@mock.patch('validate.validate.validate_checksums')
+@mock.patch('validate.validate.CHECK_FUNCTION_SWITCH')
+def test__validate_file__checks_compression(
+    mock_check_function_switch,
+    mock_validate_checksums,
+    mock_check_compressed,
+    mock_path_exists,
+    test_file_types):
+    mock_validate_checksums.return_value = None
+    mock_path_exists.return_value = True
+    mock_check_function_switch.return_value = {}
+
+    validate_file('', test_file_types, 'ext', None)
+
+    mock_check_compressed.assert_called_once()
