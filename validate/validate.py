b122a3b414ebf6129a8bfc0e2bd5f6f6eda03d75
Yash Patel <yashpatel@mednet.ucla.edu>
2023-03-08 17:21:44 -0800

    Update validator to pass args to check functions

--- validate/validate.py
+++ validate/validate.py
@@ -1,9 +1,11 @@
 ''' File validation functions '''
 from pathlib import Path
 import sys
+import argparse
 
 from validate.validators.bam import check_bam
 from validate.validators.sam import check_sam
+from validate.validators.cram import check_cram
 from validate.validators.vcf import check_vcf
 from validate.files import (
     check_compressed,
@@ -13,8 +15,9 @@ from generate_checksum.checksum import validate_checksums
 
 # Currently supported data types
 FILE_TYPES_DICT = {
-    'file-bam': ['.bam', '.cram'],
+    'file-bam': ['.bam'],
     'file-sam': ['.sam'],
+    'file-cram': ['.cram'],
     'file-vcf': ['.vcf', '.vcf.gz'],
     'file-fasta': ['.fasta', '.fa'],
     'file-fastq':['.fastq', '.fq.gz', '.fq', '.fastq.gz'],
@@ -25,11 +28,12 @@ UNKNOWN_FILE_TYPE = 'file-unknown' # file type is unlisted
 CHECK_FUNCTION_SWITCH = {
     'file-bam': check_bam,
     'file-sam': check_sam,
+    'file-cram': check_cram,
     'file-vcf': check_vcf
 }
 CHECK_COMPRESSION_TYPES = ['file-vcf', 'file-fastq', 'file-bed']
 
-def validate_file(path:Path, file_type:str, file_extension:str):
+def validate_file(path:Path, file_type:str, file_extension:str, args:argparse.Namespace):
     ''' Validate a single file '''
     path_exists(path)
 
@@ -40,8 +44,8 @@ def validate_file(path:Path, file_type:str, file_extension:str):
         check_compressed(path, file_extension)
 
     validate_checksums(path)
-
-    CHECK_FUNCTION_SWITCH.get(file_type, lambda p: None)(path)
+    print('checking')
+    CHECK_FUNCTION_SWITCH.get(file_type, lambda p, a: None)(path, args)
 
 def print_error(path:Path, err:BaseException):
     ''' Prints error message '''
@@ -82,7 +86,7 @@ def run_validate(args):
     for path in [Path(pathname) for pathname in args.path]:
         try:
             file_type, file_extension = detect_file_type_and_extension(path)
-            validate_file(path, file_type, file_extension)
+            validate_file(path, file_type, file_extension, args)
         except FileNotFoundError as file_not_found_err:
             print(f"Warning: {str(path)} {str(file_not_found_err)}")
         except (TypeError, ValueError, IOError, OSError) as err:
